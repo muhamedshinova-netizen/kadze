fff7c6296e9c07385718bac819c122cd31b429e8
muhamedshinova-netizen <muhamedshinova@gmail.com>
2026-07-06 20:31:14 +0300

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -1,5 +1,5 @@
 from supabase import create_client
-from config import SUPABASE_URL, SUPABASE_KEY
+from config import SUPABASE_URL, SUPABASE_KEY, FREE_GENERATIONS_PER_DAY
 from datetime import date
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
